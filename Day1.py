271064b5b3257dd9ba99e42801ac87aaf21d67f6
sheel-s <47894043+sheel-s@users.noreply.github.com>
2020-11-06 14:59:25 -0500

    Day 1 Assignment submitted

---
+++ Day1.py
@@ -0,0 +1,41 @@
+'''
+Created on Nov 6, 2020
+
+
+'''
+# question 1 
+answer B RAINBOW
+
+#question 2
+print("LETS UPGRADE")
+
+#question 3
+costPrice=30
+sellPrice=40
+if costPrice<sellPrice:
+    print("Profit")
+elif costPrice>sellPrice:
+    print("loss")
+else :
+    print("Neither")
+
+
+#question 4
+
+
+costPrice=input()
+sellPrice=input()
+if costPrice<sellPrice:
+    print("Profit")
+elif costPrice>sellPrice:
+    print("loss")
+else :
+    print("Neither")
+    
+  
+#question 5
+InputRupees=int(input())
+outputEuro=InputRupees*80
+print(outputEuro)
+ 
+    
